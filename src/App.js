4b70b00decde27ada27a4ed07920a1e1e486d427
Apdet <deterding1.a@gmail.com>
2020-09-03 16:31:40 -0500

    app working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { useDataLayerValue } from './DataLayer';
+import Player from './Player';
+import { getTokenFromUrl } from './spotify';
 import './App.css';
 import Login from './Login';
 
+const spotify = new SpotifyWebApi();
+
 function App() {
+  const [{ token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      spotify.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.getPlaylist("37i9dQZEVXcPwib7b4ZlD6").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+
+      spotify.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: spotify,
+      });
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+    }
+  }, [token, dispatch]);
+
   return (
-    <div className='app'>
-      <Login />
+    <div className="app">
+      {!token && <Login />}
+      {token && <Player spotify={spotify} />}
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './Login.css';
+import { loginUrl } from './spotify';
 
-function Login () {
-    return (
-        <div className="login">
-          <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-              alt=""
-          />
-          <button>LOGIN WITH SPOTIFY</button>
-
-
-        </div>
-    )
+function Login() {
+  return (
+    <div className="login">
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <a href={loginUrl}>LOGIN TO SPOTIFY</a>
+    </div>
+  );
 }
 
 export default Login;
